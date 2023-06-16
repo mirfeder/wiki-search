b1995bdf7f9a9ec4d501ab80feb9081a7b529b29
mirfeder <mirfeder@gmail.com>
2023-06-15 23:41:21 -0400

    add documentation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # wiki-search
-Search wikipedia for page views and most popular sites
+Search wikipedia for most visited pages by week or month
+
+# Installation
+- clone the project
+- cd to project root directory
+## Client Environment Setup
+- `yarn add --dev parcel` or
+- `npm install --save-dev parcel`
+
+## Server Environment Setup
+- Note: project was built using python 3.8.16
+### - activate the virtual environment
+- `cd src/server` 
+- `source venv/bin/activate`
+### - install dependencies
+- `pip install -r requirements.txt`
+## Run the Server:
+- `uvicorn main:app --reload`
+- Swagger docs available at http://localhost:8000/docs
+- OpenAPI docs available at http://localhost:8000/reDoc
+## Run the Client:
+- `yarn parcel src/client/index.html`
+- Open browser to http://localhost:1234
+
+## Using the application
+- Select either Month or Week button at the top of the left grid
+- Select desired month or week to search (future dates and dates earlier than May 1, 2015 cannot be searched)
+- Results are returned in the grid on the right
+- Select a row from the results grid to see page view metrics for that page over the selected month or week
--- src/server/main.py
+++ src/server/main.py
@@ -7,6 +7,11 @@ import requests
 app = FastAPI()
 
 class Days(BaseModel):
+    """list of days to search
+
+    Args:
+        dates (list['yyyy/mm/dd']): a list of dates in formation 'yyyy/mm/dd'
+    """
     dates: List[str]
 
 baseUrlStart = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/'
@@ -16,16 +21,35 @@ articleViewsUrl =  baseUrlStart + 'per-article' + baseUrlEnd
 
 @app.get("/monthly/{year}/{month}")
 def getMonthly(year, month):
+    """receives a year and month to search for most viewed pages during that time period
+
+    Args:
+        year (string): year to search
+        month (string): month to search
+
+    Returns:
+        dict: a dictionary with key = 'pageViews' and value = a list of dicts with information about top 1000 most viewed articles for that time period
+    """
     fullUrl = f'{topViewsUrl}{year}/{month}/all-days'
     headers = {'User-Agent': 'mirfeder@gmail.com'}
     pageViews = requests.get(fullUrl, headers=headers)
     pageViews = pageViews.json()
     return {'pageViews': pageViews['items'][0]['articles']}
 
+
 @app.get("/article/{article}/{startDate}/{endDate}")
 def getArticle(article, startDate, endDate):
+    """retrieves metrics for an article within the dates specified
+
+    Args:
+        article (string): name of article to search
+        startDate (yyyymmdd): start date of search
+        endDate (yyyymmdd): end date of search
+
+    Returns:
+       dict: a dictionary with key = 'pageViews' and value = a list of dicts with information about top 1000 most viewed articles for that time period
+    """
     fullUrl = f'{articleViewsUrl}' + '/'.join(['all-agents', article, 'daily', startDate, endDate])
-    print(fullUrl)
     headers = {'User-Agent': 'mirfeder@gmail.com'}
     pageViews = requests.get(fullUrl, headers=headers)
     if pageViews.status_code != 200:
@@ -37,6 +61,16 @@ def getArticle(article, startDate, endDate):
 
 @app.post("/weekly")
 async def getWeekly(body: Days):
+    """takes a list of dates and returns most visited pages for that time period. 
+    Dates are assumed to be consecutive but this is not validated. Results will 
+    reflect pages with the highest aggregated views over the given dates
+
+    Args:
+        body (Days): a list of dates to search in the format "yyyy/mm/dd"
+
+    Returns:
+        dict: a dictionary with key = 'pageViews' and value = a list of tuples with article name and view count, reverse-sorted by number of views
+    """
     mem = defaultdict(int)
     for day in body.dates:
         url = topViewsUrl + day
